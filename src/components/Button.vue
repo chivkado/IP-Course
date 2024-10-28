3c55211e01143e246b4e359cb808c1b0099e813f
chivkado <163482472+chivkado@users.noreply.github.com>
2024-10-28 13:50:20 +0700

    commit TP01 ex4

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="shop-button">
+        {{ buttonText }} <i class="bi bi-arrow-right"></i></button>
+</template>
+
+<script>
+export default {
+    // name: "Button",
+        props: {
+            buttonText: {
+                type: String,
+                Required: true,
+            },
+    },
+}
+</script>
+
+<style  scoped>
+    .shop-button{
+        width: 100px;
+        height: 30px;
+        background-color: #3b4cb7;
+        border-radius: 5px;
+        font-size: 13px;
+        color: white;
+        margin-top: 45%;
+        position: absolute;
+        border: none;
+        margin-left: 10%;
+    }
+    .shop-button:hover{
+        background-color: #40fdbb;
+    }
+</style>
---
+++ src/components/Promotion.vue
@@ -0,0 +1,55 @@
+<template>
+    <div  class="Promotion" :style="{backgroundColor: bgColorSecond}">
+        <img :src="bgImage" alt="">
+        <h2>{{ TittlePromotion }}</h2>
+        <Button :buttonText="'Show Now'"/>
+
+    </div>
+</template>
+
+<script>
+import Button from './Button.vue';
+export default {
+    name: "Promotion",
+    components:{
+        Button,
+    },
+    props:{
+        bgImage:String,
+        bgColorSecond:String,
+        TittlePromotion: String,
+
+    }
+}
+</script>
+
+<style scoped>
+    .Promotion{
+    width: 23.5rem;
+    height: 16rem;
+    background-color: rgb(62, 169, 45);
+    margin-top: 2.1rem;
+    margin-left: end;
+    border-radius: 20px;
+    position: relative;
+    }
+    .Promotion img{
+        width:75%;
+        height: 80%;
+        object-fit: contain;
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        
+    }
+    .Promotion h2{
+        color: black;
+        position: absolute;
+        top: 15%;
+        left: 10%;
+        width: 50%;
+        font-size: 20px ;
+
+    }
+
+</style>
